9b98e704f64a97d6601a23094a549e324d5f2ee6
nihar-mukhiya <42922931+nihar-mukhiya@users.noreply.github.com>
2018-09-30 11:27:04 +0530

    generalized finding root of decision tree procedure works for any column selected as class

--- decision-tree-generalized.py
+++ decision-tree-generalized.py
@@ -2,14 +2,15 @@ import tkinter as tk
 from tkinter import filedialog
 from datascience import *
 from pandas import *
+import copy
 import numpy as np
 from math import log2
 
 def ent(list):
     entropy, g = 0, 0
     for z in list:
-        entropy += - list[g] * log2(list[g])
-        print(entropy)
+        entropy = entropy + (- list[g] * log2(list[g]))
+        g+=1
     return entropy
 
 root = tk.Tk()
@@ -29,15 +30,67 @@ for x in range(0, len(column_names)):
 clas = int(input("select the column to be selected as class: "))
 #uniq contains unique values in our class
 uniq = e[column_names[clas - 1]].unique()
+l = len(uniq)
 # p_class will contain probabilities of values in our selected class column
 p_class = []
+
 for y in range(0, len(uniq)):
     clas1 = a.select(column_names[clas - 1]).where(column_names[clas - 1], uniq[y]).num_rows
     clas1 = clas1 / c
     p_class.append(clas1)
-print(p_class)
+
 entropy_val = ent(p_class)
-print(entropy_val)
+print("Entropy for your class is: " +str(entropy_val))
+temp = copy.deepcopy(list(column_names))
+temp.remove(temp[clas - 1])
+#print(temp)
+prob_of_column = {}
+prob_of_values_in_column = {}
+info = {}
+temp2 = []
+for z in range(0, len(temp)):           #len(temp)
+    uniq1 = e[temp[z]].unique()
+
+    clas3 = []
+
+    clas5 = []
+    for y in range(0, len(uniq1)):
+        clas4 = []
+        clas5 = a.where(temp[z], uniq1[y])
+        clas2 = a.select(temp[z]).where(temp[z], uniq1[y]).num_rows
+        val = clas2
+        clas2 = clas2 / c
+        clas3.append(clas2)
+        for b in range(0, len(uniq)):
+            temp1 = clas5.where(column_names[clas - 1], uniq[b]).num_rows
+            temp1 = temp1 / val
+            clas4.append(temp1)
+            prob_of_values_in_column[uniq1[y]] = clas4
+    prob_of_column[temp[z]] = clas3
+
+    templist = []
+    for h in range(0, len(prob_of_column[temp[z]])):
+            temp5 = 0
+            j1 = prob_of_column[temp[z]][h]
+            temp4 = 0
+            for h2 in range(0, len(uniq)):
+                j2 = prob_of_values_in_column[uniq1[h]][h2]
+                try:
+                    temp5 = temp5 + (-(j2 * log2(j2)))
+                except ValueError:
+                    temp5 = 0
+                temp4 = j1 * temp5
+            templist.append(temp4)
+    sum1 = sum(templist)
+    sum1 = entropy_val - sum1
+    info[temp[z]] = sum1
+
+print(info)
+root = max(info, key=info.get)
+print("Root of the decision tree is: " +str(root))
+
+
+
 
 
 
